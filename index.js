1b0caf7eb86471c0616a820238b48fd1b3fea5db
theonetheycallneo <anthony.wentzel@gmail.com>
2018-09-21 12:11:25 -0700

    feat: adding js wrapper

--- index.js
+++ index.js
@@ -1,6 +1,66 @@
+import { NativeModules } from 'react-native'
+import { NativeAppEventEmitter } from 'react-native'
 
-import { NativeModules } from 'react-native';
+const { RNUberSSO } = NativeModules
 
-const { RNUberSSO } = NativeModules;
+const uber = {}
 
-export default RNUberSSO;
+const eventsMap = {}
+
+uber.initSdk = (options, successC, errorC) => {
+  // console.log("initSdk: " + eventsMap['onSSOUberSSOAccessToken']);
+  // options.isDebug = eventsMap['isDebug']
+  //   ? true
+  //   : false
+  return RNUberSSO.initSdk(options, successC, errorC)
+}
+
+uber.login = () => {
+  return RNUberSSO.login()
+}
+
+/**
+ * Accessing Uber Attribution / Conversion Data from the SDK (Deferred Deeplinking)
+ * @param callback: contains fields:
+ *    status: success/failure
+ *    type:
+ *          onSSOUberSSOAccessToken
+ *    data: metadata,
+ * @example {"status":"success","type":"onSSOUberSSOAccessToken","data":"accessToken"}
+ *
+ * @returns {remove: function - unregister listener}
+ */
+uber.onSSOUberSSOAccessToken = callback => {
+  //console.log("onSSOUberSSOAccessToken is called" );
+
+  const listener = NativeAppEventEmitter.addListener(
+    'onSSOUberSSOAccessToken',
+    _data => {
+      if (callback && typeof callback === typeof Function) {
+        try {
+          let data = JSON.parse(_data)
+          callback(data)
+        } catch (_error) {
+          //throw new UberParseJSONException("...");
+          //TODO: for today we return an error in callback
+          callback(new UberParseJSONException('Invalid data structure', _data))
+        }
+      }
+    }
+  )
+
+  eventsMap['onSSOUberSSOAccessToken'] = listener
+
+  // unregister listener (suppose should be called from componentWillUnmount() )
+  return function remove() {
+    listener.remove()
+  }
+}
+
+function UberParseJSONException(_message, _data) {
+  this.message = _message
+  this.data = _data
+  this.name = 'UberParseJSONException'
+}
+
+export default uber
