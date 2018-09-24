1f15d2de600e853756d8b77eff4af78e39406544
theonetheycallneo <anthony.wentzel@gmail.com>
2018-09-24 15:27:31 -0700

    feat(ios): adding uber core 0.10.0

--- ios/RNUberSSO.h
+++ ios/RNUberSSO.h
@@ -8,4 +8,16 @@
 @interface RNUberSSO : NSObject <RCTBridgeModule>
 
 @end
-  
+
+// Appsflyer JS objects
+#define uberClientId                    @"clientId"
+#define uberEnvironment                 @"environment"
+#define uberIsDebug                     @"isDebug"
+#define uberRedirectUri                 @"redirectUri"
+
+// Appsflyer native objects
+#define uberSuccess                     @"success"
+#define uberFailure                     @"failure"
+#define uberOnUberSSOFailure            @"onUberSSOFailure"
+#define uberOnUberSSOSuccess            @"onUberSSOSuccess"
+#define uberOnSSOAccessToken            @"onSSOUberSSOAccessToken"
--- ios/RNUberSSO.m
+++ ios/RNUberSSO.m
@@ -1,14 +1,14 @@
 
 #import "RNUberSSO.h"
 
+@import UberCore;
+
 @implementation RNUberSSO
 
 @synthesize bridge = _bridge;
 
-static NSString *const NO_DEVKEY_FOUND              = @"No 'devKey' found or its empty";
-static NSString *const NO_APPID_FOUND               = @"No 'appId' found or its empty";
-static NSString *const NO_EVENT_NAME_FOUND          = @"No 'eventName' found or its empty";
-static NSString *const NO_EMAILS_FOUND_OR_CORRUPTED = @"No 'emails' found, or list is corrupted";
+static NSString *const NO_CLIENT_ID_FOUND           = @"No 'clientId' found or its empty";
+static NSString *const NO_REDIRECT_URI_FOUND        = @"No 'redirectUri' found or its empty";
 static NSString *const SUCCESS                      = @"Success";
 
 - (dispatch_queue_t)methodQueue
@@ -22,54 +22,64 @@ RCT_EXPORT_METHOD(initSdk: (NSDictionary*)initSdkOptions
                   errorCallback:(RCTResponseErrorBlock)errorCallback
                   )
 {
+  NSString* clientId = nil;
+  NSString* redirecUri = nil;
+  NSString* environment = nil;
+  BOOL isDebug = NO;
+
+  if (![initSdkOptions isKindOfClass:[NSNull class]]) {
+      id isDebugValue = nil;
+      clientId = (NSString*)[initSdkOptions objectForKey: uberClientId];
+      environment = [initSdkOptions objectForKey: uberEnvironment];
+      redirecUri = (NSString*)[initSdkOptions objectForKey: uberRedirectUri];
+      isDebugValue = [initSdkOptions objectForKey: uberIsDebug];
+      if ([isDebugValue isKindOfClass:[NSNumber class]]) {
+          // isDebug is a boolean that will come through as an NSNumber
+          isDebug = [(NSNumber*)isDebugValue boolValue];
+      }
+  }
+
+  NSError* error = nil;
+
+  if (!clientId || [clientId isEqualToString:@""]) {
+    error = [NSError errorWithDomain:NO_CLIENT_ID_FOUND code:0 userInfo:nil];
+  } else if (!redirecUri || [redirecUri isEqualToString:@""]) {
+    error = [NSError errorWithDomain:NO_REDIRECT_URI_FOUND code:1 userInfo:nil];
+  }
+
+  if (!environment || [environment isEqualToString:@""]) {
+    environment = @"sandbox";
+  }
+
+  if (error != nil) {
+    errorCallback(error);
+  } else {
+    UBSDKConfiguration* configuration = UBSDKConfiguration.shared;
+    [configuration setClientID:clientId];
+    [configuration setCallbackURI:[NSURL URLWithString:redirecUri]];
+    [configuration setIsSandbox:[environment isEqualToString:@"sandbox"]];
     
-    NSString* devKey = nil;
-    NSString* appId = nil;
-    BOOL isDebug = NO;
-    BOOL isConversionData = NO;
+    successCallback(@[SUCCESS]);
+  }
+}
 
-    if (![initSdkOptions isKindOfClass:[NSNull class]]) {
-
-        id isDebugValue = nil;
-        id isConversionDataValue = nil;
-        devKey = (NSString*)[initSdkOptions objectForKey: afDevKey];
-        appId = (NSString*)[initSdkOptions objectForKey: afAppId];
-
-        isDebugValue = [initSdkOptions objectForKey: afIsDebug];
-        if ([isDebugValue isKindOfClass:[NSNumber class]]) {
-            // isDebug is a boolean that will come through as an NSNumber
-            isDebug = [(NSNumber*)isDebugValue boolValue];
-        }
-        isConversionDataValue = [initSdkOptions objectForKey: afConversionData];
-        if ([isConversionDataValue isKindOfClass:[NSNumber class]]) {
-            isConversionData = [(NSNumber*)isConversionDataValue boolValue];
-        }
-    }
-
-    NSError* error = nil;
-
-    if (!devKey || [devKey isEqualToString:@""]) {
-        error = [NSError errorWithDomain:NO_DEVKEY_FOUND code:0 userInfo:nil];
-        
-    } else if (!appId || [appId isEqualToString:@""]) {
-        error = [NSError errorWithDomain:NO_APPID_FOUND code:1 userInfo:nil];
-    }
-
-    if (error != nil) {
-        errorCallback(error);
-    } else {
-        if(isConversionData == YES){
-            [AppsFlyerTracker sharedTracker].delegate = self;
-        }
-        
-        [AppsFlyerTracker sharedTracker].appleAppID = appId;
-        [AppsFlyerTracker sharedTracker].appsFlyerDevKey = devKey;
-        [AppsFlyerTracker sharedTracker].isDebug = isDebug;
-        [[AppsFlyerTracker sharedTracker] trackAppLaunch];
-        
-        successCallback(@[SUCCESS]);
-    }
+RCT_EXPORT_METHOD(login)
+{
+  UBSDKLoginManager *loginManager = [[UBSDKLoginManager alloc] init];
+  NSArray *scopes = @[
+    UBSDKScope.history,
+    UBSDKScope.places,
+    UBSDKScope.profile,
+    UBSDKScope.allTrips,
+    UBSDKScope.requestReceipt,
+    UBSDKScope.request ];
+  UIViewController *rootViewController = [UIApplication sharedApplication].delegate.window.rootViewController;
+  [loginManager loginWithRequestedScopes:scopes
+                presentingViewController: rootViewController
+                              completion: ^(UBSDKAccessToken * _Nullable accessToken, NSError * _Nullable error) {
+                                // Completion block. If accessToken is non-nil, you're good to go
+                                // Otherwise, error.code corresponds to the RidesAuthenticationErrorType that occured
+                              }];
 }
 
 @end
-  
